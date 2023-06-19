0d67678745a8812c96c635cd2a5dd8a4c76f1408
Andrii Cherevatyi <andrecherevat@gmail.com>
2023-06-19 16:37:02 +0200

    add Book.java class with first attributes and empty Author.java

---
+++ src/main/java/ua/cupofbooks/Author.java
@@ -0,0 +1,4 @@
+package ua.cupofbooks;
+
+public class Author {
+}
---
+++ src/main/java/ua/cupofbooks/Book.java
@@ -0,0 +1,14 @@
+package ua.cupofbooks;
+
+import java.time.LocalDate;
+
+public class Book {
+
+    private int bookID;
+    private String title;
+    private Author author;
+    private String genre;
+    private String description;
+    private LocalDate releaseDate;
+
+}
